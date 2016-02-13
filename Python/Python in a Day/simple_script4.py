956df1b7ed8dad339b75066e67d1ef5322113701
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-13 10:54:34 -0800

    Added a simple script from Python in a Day

---
+++ Python/Python in a Day/simple_script4.py
@@ -0,0 +1,20 @@
+### Start counter at 0
+##counter = 0
+##
+### While 'counter' is less than or equal to 5, run the loop
+##
+##while counter < 5:
+##    # Show counter value
+##    print counter
+##    # the long way to increase counter by 1
+##    # counter = counter + 1
+##    # shortcut to increase counter by 1
+##    counter += 1
+##print counter
+
+# Run loop in range 0 - 5
+# Counter changes automatically in each iteration
+for counter in range(0,5):
+    # Show the value of counter
+    print counter
+    
