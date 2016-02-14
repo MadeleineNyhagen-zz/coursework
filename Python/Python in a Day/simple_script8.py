7858989fe2e8acfe51d96f65ad728a94b4e4b04f
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-14 11:49:38 -0800

    Added simple script from Python in a Day

---
+++ Python/Python in a Day/simple_script8.py
@@ -0,0 +1,16 @@
+##from math import sqrt, exp
+##
+##print sqrt(16)
+##print exp(2)
+
+# import our module
+import smallMathsModule
+
+# multiplyBy5 function
+print smallMathsModule.multiplyBy5(3)
+
+# add5 function
+print smallMathsModule.add5(9)
+
+# randomAdd function
+print smallMathsModule.randomAdd(8)
