1d10bd6e33b452b525761e518794ff0b43466323
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-17 19:03:58 -0800

    Adding course file

---
+++ Python/Lynda Python Course/Python3EssentialTraining20-generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# generator.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    print("This is the functions.py file.")
+    for i in inclusive_range(25):
+        print(i, end = ' ')
+
+def inclusive_range(start, stop, step):
+
+if __name__ == "__main__": main()
