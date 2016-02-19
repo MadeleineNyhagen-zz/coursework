75666694bd2acccc5434573817b05ac1998942df
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-18 16:42:19 -0800

    Adding course file

--- Python/Lynda Python Course/Python3EssentialTraining20-generator.py
+++ Python/Lynda Python Course/Python3EssentialTraining20-generator.py
@@ -5,9 +5,40 @@
 
 def main():
     print("This is the functions.py file.")
-    for i in inclusive_range(25):
+    for i in range(25): # only prints through 24
+        print(i, end = ' ')
+    print()
+
+    for i in inclusive_range(0, 25, 1):
+        print(i, end = ' ')
+    print()
+
+    for i in inclusive_rangeTwo(25):
         print(i, end = ' ')
 
+# a function that will yield an inclusive range:
 def inclusive_range(start, stop, step):
+    i = start
+    while i <= stop:
+        yield i
+        i += step
+
+def inclusive_rangeTwo(*args):
+    numargs = len(args)
+    if numargs < 1: raise TypeError('requires at least one argument')
+    elif numargs == 1:
+        stop = args[0]
+        start = 0
+        step = 1
+    elif numargs == 2:
+        (start, stop) = args
+        step = 1
+    elif numargs == 3:
+        (start, stop, step) = args
+    else: raise TypeError('inclusive_range expected at most 3 arguments, got {}'.format(numargs))
+    i = start
+    while i <=stop:
+        yield i # yield will return a generator
+        i += step
 
 if __name__ == "__main__": main()
