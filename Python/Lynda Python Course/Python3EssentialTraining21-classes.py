b6d2c4d2a93f957a07d8c2a8e0d443fc7c14b620
Madeleine Nyhagen <m.l.nyhagen@gmail.com>
2016-02-20 13:54:41 -0800

    Updated file

--- Python/Lynda Python Course/Python3EssentialTraining21-classes.py
+++ Python/Lynda Python Course/Python3EssentialTraining21-classes.py
@@ -4,7 +4,7 @@
 # Copyright 2010 The BearHeart Group, LLC
 
 class Duck:
-    def __init__(self, value):
+    def __init__(self, value = '1'):
         self._v = value
         print('A duck is born.')
         
@@ -14,13 +14,37 @@ class Duck:
     def walk(self):
         print('Walks like a duck.', self._v)
 
+class DuckTwo:
+    def __init__(self, color = 'white'):
+        self._color = color
+    
+    def quack(self):
+        print('Quaaack!')
+
+    def walk(self):
+        print('Walks like a duck.')
+
+    def set_color(self, color):
+        self._color = color
+
+    def get_color(self):
+        return self._color
+    
+
 def main():
-    donald = Duck(47)
+    donald = Duck()
     donald.quack()
     donald.walk()
+    
     howard = Duck(131)
     howard.quack()
     howard.walk()
+    
+    daffy = DuckTwo()
+    print(daffy.get_color())
+    daffy.set_color('black')
+    print(daffy.get_color())
+
     
 
 if __name__ == "__main__": main()
