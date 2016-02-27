f0fa851b938ebf73ee0681c905bf28d8377ecce5
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-27 14:02:06 -0800

    Added course file

--- Python/Lynda Python Course/Python3EssentialTraining25-containers.py
+++ Python/Lynda Python Course/Python3EssentialTraining25-containers.py
@@ -72,40 +72,58 @@ def main():
 ##    print(x2.pop(0)) # pops first item in x2
 ##    print(x2)
 
-    # from the third video on containers:
-
-    d = {'one': 1, 'two': 2, 'three': 3} # creates a dictionary
-    print(d)
-
-    d2 = dict(one = 1, two = 2, three = 3) # this does the exact same as above, but is easier to type
-    print(d2)
-    print(type(d), type(d2))
-
-    d3 = dict(four = 4, five = 5, six = 6)
-    print(d3)
-
-    d4 = dict(one = 1, two = 2, three = 3, **d3)
-    print(d4)
-
-    print('four' in d3)
-    print('three' in d3)
-
-    for k in d4: print(k)
-    for k, v in d4.items(): print(k, v)
-
-    print(d4['three']) # returns the value associated with the 'three' key
-    # print(d3['three']) will raise an exception, because d3 doesn't have a 'three'
-    print(d3.get('three')) # will instead return 'None' if there is no match
-    print(d4.get('three')) # returns the value associated with 'three'
-    print(d3.get('three', 'not found')) # returns 'not found' instead of 'None' if 'three' isn't found in the dictionary
-
-    del d3['four'] # deletes 'four' and its associated value from the dictionary
-    print(d3)
-    print(d3.pop('five')) # pops 'five' and its associated value from the dictionary
-    print(d3)
+##    # from the third video on containers:
+##
+##    d = {'one': 1, 'two': 2, 'three': 3} # creates a dictionary
+##    print(d)
+##
+##    d2 = dict(one = 1, two = 2, three = 3) # this does the exact same as above, but is easier to type
+##    print(d2)
+##    print(type(d), type(d2))
+##
+##    d3 = dict(four = 4, five = 5, six = 6)
+##    print(d3)
+##
+##    d4 = dict(one = 1, two = 2, three = 3, **d3)
+##    print(d4)
+##
+##    print('four' in d3)
+##    print('three' in d3)
+##
+##    for k in d4: print(k)
+##    for k, v in d4.items(): print(k, v)
+##
+##    print(d4['three']) # returns the value associated with the 'three' key
+##    # print(d3['three']) will raise an exception, because d3 doesn't have a 'three'
+##    print(d3.get('three')) # will instead return 'None' if there is no match
+##    print(d4.get('three')) # returns the value associated with 'three'
+##    print(d3.get('three', 'not found')) # returns 'not found' instead of 'None' if 'three' isn't found in the dictionary
+##
+##    del d3['four'] # deletes 'four' and its associated value from the dictionary
+##    print(d3)
+##    print(d3.pop('five')) # pops 'five' and its associated value from the dictionary
+##    print(d3)
 
     # from the fourth video on containers
+
+    # reading a utf-8 file
+
+    fin = open('utf8.txt', 'r', encoding = 'utf_8')
+    fout = open('utf8.html', 'w')
+    outbytes = bytearray()
+    for line in fin:
+        for c in line:
+            if ord(c) > 127: # because there are 128 (0-127) values in utf-8 that are normal ascii characters
+                # if the character is outside of the normal ascii range, we encode it with an xml entity (passed into 'bytes()'),
+                # which can be used in an html context
+                outbytes += bytes('&#{:04d};'.format(ord(c)), encoding = 'utf_8')
+            else: outbytes.append(ord(c)) # if the character is within the normal ascii range, it is appended to outbytes
+    outstr = str(outbytes, encoding = 'utf_8')
+    print(outstr, file = fout)
+    print(outstr)
+    print('Done.')
     
+
     
 
     
