5f0d834a17831e6bc16b691f540516afa3865d0f
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-26 17:33:26 -0800

    Updated course file

--- Python/Lynda Python Course/Python3EssentialTraining24-strings.py
+++ Python/Lynda Python Course/Python3EssentialTraining24-strings.py
@@ -20,6 +20,29 @@ def main():
     s1 = 'this is a string\n'
     print(s1)
     print(s1.rstrip()) # can specify a string to be stripped
-    print(s1)
+
+    a, b = 5, 42
+    print('this is {}, that is {}'.format(a, b))
+    print('this is {0}, that is {1}, this is also {1}, and that is {0}'.format(a,b))
+    print('this is {bob} and that is {fred}'.format(bob = a, fred = b))
+    s2 = 'this is {}, that is {}'
+    print(s2.format(a,b))
+
+    d = dict( bob = a, fred = b )
+    'this is {bob}, this is {fred}'.format(**d)
+
+    s3 = 'This is a string of words'
+    print(s3)
+    print(s3.split())
+    print(s3.split('i'))
+    words = s3.split()
+    print(words)
+    for w in words: print(w)
+    new = ':'.join(words)
+    print(new)
+    new2 = s3.center(80)
+    print(new2)
+
+    # to learn more about string methods, go to http://docs.python.org/py3k/library/stdtypes.html#string-methods
 
 if __name__ == "__main__": main()
