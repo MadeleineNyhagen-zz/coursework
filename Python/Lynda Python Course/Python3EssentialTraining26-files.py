7f6d13a6ce013f434a2f070703ce731ce542b001
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-27 17:15:26 -0800

    Updated course file

--- Python/Lynda Python Course/Python3EssentialTraining26-files.py
+++ Python/Lynda Python Course/Python3EssentialTraining26-files.py
@@ -4,8 +4,34 @@
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    f = open('lines.txt')
-    for line in f:
-        print(line, end = '')
+
+    # to read a file:
+    
+##    f = open('lines.txt')
+##    for line in f:
+##        print(line, end = '')
+    
+    # to copy the contents of a file to a new file:
+
+##    infile = open('lines.txt', 'r')
+##    outfile = open('new.txt', 'w')
+##    for line in infile:
+##        print(line, file = outfile, end = '')
+##    print('Done.')
+
+    # reading/writing a large file:
+
+##    buffersize = 50000
+##    infile = open('bigfile.txt', 'r')
+##    outfile = open('new.txt', 'w')
+##    buffer = infile.read(buffersize) # the read method isn't iterable, so we will have to use
+##    # a while loop instead of a for loop:
+##    while len(buffer):
+##        outfile.write(buffer)
+##        print('.', end = '')
+##        buffer = infile.read(buffersize)
+##    print()
+##    print('Done.')
+
 
 if __name__ == "__main__": main()
