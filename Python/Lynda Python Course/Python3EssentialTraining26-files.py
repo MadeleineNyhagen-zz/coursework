d4c538fa3dcf7b9089d4d32f6a7120aa1411b3e8
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-02-27 17:28:17 -0800

    Updated course file

--- Python/Lynda Python Course/Python3EssentialTraining26-files.py
+++ Python/Lynda Python Course/Python3EssentialTraining26-files.py
@@ -5,11 +5,15 @@
 
 def main():
 
+    # from the first video on files:
+
     # to read a file:
     
 ##    f = open('lines.txt')
 ##    for line in f:
 ##        print(line, end = '')
+
+    # from the second video on files:
     
     # to copy the contents of a file to a new file:
 
@@ -32,6 +36,21 @@ def main():
 ##        buffer = infile.read(buffersize)
 ##    print()
 ##    print('Done.')
+
+    # from the third video on files:
+
+    #reading/writing a binary file, must be done using buffered I/O:
+    
+    buffersize = 50000
+    infile = open('olives.jpg', 'rb') # the 'rb' puts it in read binary mode
+    outfile = open('new.jpg', 'wb') # 'wb' puts it in write binary mode
+    buffer = infile.read(buffersize)
+    while len(buffer):
+        outfile.write(buffer)
+        print('.', end = '')
+        buffer = infile.read(buffersize)
+    print()
+    print('Done.')
 
 
 if __name__ == "__main__": main()
