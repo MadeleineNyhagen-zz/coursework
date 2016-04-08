a9cbb3e44c971c37becdad327043704e2d2d5e1b
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-07 18:09:02 -0700

    updated file

---
+++ Python/Drills/File Transfer Drills/File Transfer GUI DB/PyDrill_db.py
@@ -0,0 +1,50 @@
+import sqlite3
+from datetime import datetime
+from datetime import timedelta
+import calendar
+
+# connect to timestamp database
+conn = sqlite3.connect('timestamp.db')
+
+def createTable():
+    conn.execute("CREATE TABLE if not exists \
+        Time_Stamps(Time_Checked INTEGER);")
+
+def addTimeStamp():
+    d = datetime.utcnow()
+    timestamp = calendar.timegm(d.utctimetuple())
+    inserttime = "INSERT INTO Time_Stamps \
+        (Time_Checked) VALUES ({})".format(timestamp)
+    #print timestamp
+    #print inserttime
+    conn.execute(inserttime)
+    conn.commit()
+
+# to return the time 
+
+def lastChecked():
+    lastcheck = conn.execute("SELECT MAX(Time_Checked) FROM Time_Stamps")
+    for row in lastcheck:
+        timestamp = datetime.fromtimestamp(row[0])
+        #print(timestamp)
+##        print(row[0])
+    return(timestamp)
+
+def viewTable():
+    vt = conn.execute("SELECT * FROM Time_Stamps")
+    for row in vt:
+        print(row)
+    print(vt)
+
+    
+#addTimeStamp()
+#print(datetime.now()-lastChecked())
+#lastChecked()
+#viewTable()
+
+##d = datetime.utcnow()
+##timestamp = calendar.timegm(d.utctimetuple())
+##ft = datetime.fromtimestamp(timestamp)
+##print(d)
+##print(timestamp)
+##print(ft)
