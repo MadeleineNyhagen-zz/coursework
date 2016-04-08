4b867a5fdd3943d636a139e67428a28b50f9b2bc
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-07 18:11:31 -0700

    Updated GUI

--- Python/Drills/HTML generator GUI/PyDrill_gui_34.py
+++ Python/Drills/HTML generator GUI/PyDrill_gui_34.py
@@ -11,6 +11,22 @@ class HTMLEditor:
     def __init__(self, master):
 
         master.title('Edit the sale page! What should it say?')
+        master.configure(background = '#99ccff')
+
+        self.style = ttk.Style()
+        self.style.configure('TFrame', background = '#99ccff')
+        self.style.configure('TButton', background = '#99ccff')
+        self.style.configure('TLabel', background = '#99ccff', font = ('Helvetica', 11))
+        self.style.configure('Header.TLabel', font = ('Helvetica', 18, 'bold'))
+
+        self.frame_header = ttk.Frame(master)
+        self.frame_header.pack()
+
+        self.picture = PhotoImage(file = 'maru3.gif')
+        ttk.Label(self.frame_header, image = self.picture).grid(row = 0, column = 0, rowspan = 2)
+        ttk.Label(self.frame_header, text = 'Summer Sale Page!', style = 'Header.TLabel').grid(row = 0, column = 1)
+        ttk.Label(self.frame_header, wraplength = 300, text = ("Type out the text you'd like to see in the body of our summer sale announcement page. "
+                                            "When you're done, hit submit to create it!")).grid(row = 1, column = 1)
 
 
 
