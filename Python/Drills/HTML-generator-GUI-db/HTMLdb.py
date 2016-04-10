2d18a45483dbae67d45c7726719096d147d51570
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-09 19:52:18 -0700

    Updated file

--- Python/Drills/HTML-generator-GUI-db/HTMLdb.py
+++ Python/Drills/HTML-generator-GUI-db/HTMLdb.py
@@ -3,13 +3,36 @@ import sqlite3
 db = sqlite3.connect('htmlpage.db')
 cursor = db.cursor()
 
+##cursor.execute("DROP TABLE bodytext")
+##db.commit()
+
 def createTable():
     cursor.execute("CREATE TABLE IF NOT EXISTS \
             bodytext( \
             ID INTEGER PRIMARY KEY AUTOINCREMENT,\
-            Name TEXT, \
+            Title TEXT, \
             Content TEXT \
             );")
     db.commit()
 
 createTable()
+
+def newPage(title, content):
+    addvalues = "'{}', '{}'".format(title, content)
+    addpage = "INSERT INTO bodytext (Title, Content) VALUES \
+        ({});".format(addvalues)
+    cursor.execute(addpage)
+    db.commit()
+    return db.total_changes
+
+# newPage('test', 'this is the body')
+
+def displayAll():
+    select = "SELECT * FROM bodytext"
+    display = cursor.execute(select)
+    rows = display.fetchall()
+    return rows
+
+for item in displayAll():
+    print(item)
+    
