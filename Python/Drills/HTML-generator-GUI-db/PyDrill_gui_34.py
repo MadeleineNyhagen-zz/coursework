1459774d98790a4260bbc7dc78231ad221eb7f6a
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-16 20:56:26 -0700

    updated file

--- Python/Drills/HTML-generator-GUI-db/PyDrill_gui_34.py
+++ Python/Drills/HTML-generator-GUI-db/PyDrill_gui_34.py
@@ -12,6 +12,7 @@ class HTMLEditor:
 
         master.title('Edit the sale page! What should it say?')
         master.configure(background = '#99ccff')
+        master.geometry("500x500")
 
         self.style = ttk.Style()
         self.style.configure('TFrame', background = '#99ccff')
@@ -27,6 +28,20 @@ class HTMLEditor:
         self.frame_content = ttk.Frame(master)
         self.frame_content.pack()
 
+        self.frame_content.columnconfigure(0, weight=1)
+        self.frame_content.columnconfigure(1, weight=1)
+        self.frame_content.columnconfigure(2, weight=1)
+        self.frame_content.columnconfigure(3, weight=1)
+        self.frame_content.columnconfigure(4, weight=1)
+        self.frame_content.columnconfigure(5, weight=1)
+        self.frame_content.columnconfigure(6, weight=1)
+        self.frame_content.columnconfigure(7, weight=1)
+
+        self.frame_content.rowconfigure(0, weight = 1)
+        self.frame_content.rowconfigure(1, weight = 1)
+        self.frame_content.rowconfigure(2, weight = 1)
+        
+
         # adding content to the header
         self.picture = PhotoImage(file = 'maru3.gif')
         ttk.Label(self.frame_header, image = self.picture).grid(row = 0, column = 0, rowspan = 2)
@@ -38,15 +53,29 @@ class HTMLEditor:
         # adding content to the body
 
         # adding the name entry box
-        self.name_entry = Text(self.frame_content, width = 30, height = 1, font = ('Helvetica',10))
-        self.name_entry.grid(row = 0, column = 0, padx = 1, pady = 5, stick = 'n')
+        # label
+        self.name_label = ttk.Label(self.frame_content, width = 18, text = 'Page Title: ')
+        self.name_label.grid(row = 0, column = 0, columnspan = 2, padx = 5, pady = 5, stick = 'ne')
+        # text entry
+        self.name_entry = Text(self.frame_content, height = 1, width = 30, font = ('Helvetica',10))
+        self.name_entry.grid(row = 0, column = 2, rowspan = 1, columnspan= 3, padx = 1, pady = 5, stick = 'nw')
 
-        # adding the body text entry box        
-        self.body_text = Text(self.frame_content, width = 50, height = 10, font = ('Helvetica',10))
-        self.body_text.grid(row = 0, column = 2, columnspan = 2, padx = 1, pady = 5)
+        # adding the listbox to contain names of various page versions
+        self.name_list = Listbox(self.frame_content, height = 17, width = 30, font = ('Helvetica',10))
+        self.name_list.grid(row = 1, column = 2, columnspan = 3, rowspan = 4, padx = 1, pady = 1, stick = 'nw')
 
-        ttk.Button(self.frame_content, text = 'Submit', command = self.savebodytext).grid(row = 1, column = 0, padx = 5, stick = 'e')
-        ttk.Button(self.frame_content, text = 'Cancel', command = self.exitgui).grid(row = 1, column = 1, padx = 5, stick = 'w')
+        
+
+        # adding the body text entry box
+        #label
+        self.body_label = ttk.Label(self.frame_content, width = 20, text = 'Body Text: ')
+        self.body_label.grid(row = 0, column = 5, columnspan = 2, stick = 'ne')
+        # text entry
+        self.body_text = Text(self.frame_content, height = 20, width = 30, font = ('Helvetica',10))
+        self.body_text.grid(row = 0, column = 7, rowspan = 5, columnspan = 3, padx = 5, pady = 5, stick = 'nw')
+
+        ttk.Button(self.frame_content, text = 'Submit', command = self.savebodytext).grid(row = 6, column = 2, padx = 5, pady = 5, stick = 'nw')
+        ttk.Button(self.frame_content, text = 'Cancel', command = self.exitgui).grid(row = 6, column = 3, padx = 5, pady = 5, stick = 'ne')
 
 
 
