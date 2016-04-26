319b3fc2dc21fa5f274d1e2903b0560da6c89fbe
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-26 16:17:46 -0700

    updated file

--- JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0407.js
+++ JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0407.js
@@ -12,16 +12,16 @@ thing = {};
 typeof thing;
 
 thing = [];
-typeof thing;
+typeof thing; // since an array in JavaScript is an object this isn't helpful in distinguishing between an array and an object, but this is:
 typeof thing === 'object' && thing.hasOwnProperty('length'); // true
 
 thing = {};
-typeof thing === 'object' && thing.hasOwnProperty('length'); // true
+typeof thing === 'object' && thing.hasOwnProperty('length'); // false
 
 NaN;
-typeof NaN;
+typeof NaN; // this will, confusingly, return number
 
-typeof null;
+typeof null; // also confusingly, this will return object
 thing === null;
 thing = null;
 thing;
