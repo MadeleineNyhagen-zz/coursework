5056abaf073c253c051c2d03411d666cfc4461b1
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-26 16:54:22 -0700

    updated file

--- JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0603.js
+++ JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0603.js
@@ -1,8 +1,8 @@
 function speakSomething(what, howMany) {
 	// this pattern works nicely for default values:
 	// check if the argument is undefined, and if it is, provide a default value
-	var what = (typeof what !== 'undefined') ? what : 'Default speech';
-	var howMany = (typeof howMany !== 'undefined') ? howMany : 10;
+	var what = (typeof what !== 'undefined') ? what : 'Default speech'; // if what is undefined, provide default value
+	var howMany = (typeof howMany !== 'undefined') ? howMany : 10; // if howMany is undefined, provide default value
 	
 	for (var i = 0; i < howMany; i += 1) {
 		console.log(what + " (" + i + ")");
