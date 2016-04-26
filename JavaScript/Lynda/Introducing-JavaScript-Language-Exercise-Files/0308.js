fb22c0f701aa3272634e4f0e71596711e11674a1
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-25 18:59:03 -0700

    Organized files

--- JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0308.js
+++ JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0308.js
@@ -10,15 +10,15 @@ console.log( regex.test(string2) );
 console.log( regex.test(string3) );
 console.log( regex.test(string4) );
 
-regex = /this/i;
+regex = /this/i; // the added 'i' serves to make this regex case insensitive
 
-regex = /^this/i;
+regex = /^this/i; // the ^ indicates that we're looking for strings that begin with 'this'
 
-regex = /this$/i;
+regex = /this$/i; // the $ indicates that we're looking for strings that end with 'this'
 
-regex = /ever.$/i;
+regex = /ever.$/i; // the . stands for any character
 
-regex = /ever\.$/i;
+regex = /ever\.$/i; // to look for an actual . you can escape it with a backslash
 
 regex = /Moun.$/i;
 
