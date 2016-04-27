d5f316cdc06b863af07ce88c4e808f4428845c19
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-04-26 17:26:01 -0700

    updated file

--- JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0606.js
+++ JavaScript/Lynda/Introducing-JavaScript-Language-Exercise-Files/0606.js
@@ -2,7 +2,9 @@ var myNum = 32;
 var myResult = 'Success!';
 
 function randomizer(limit) {
-	var myNum = Math.floor(Math.random() * limit);
+	var myNum = Math.floor(Math.random() * limit); // this creates a local myNum variable within the function, and does not affect the global myNum
+	// if myNum is referenced within the function without being declared again, it will use the global myNum:
+	// myNum = Math.floor(Math.random() * limit);
 	
 	console.log('myNum is ' + myNum);
 	console.log('Global myNum is ' + window.myNum);
