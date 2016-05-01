aaf627db6778cabde3fc5c375a37e22e7f810660
Madeleine <madeleinenyhagen@users.noreply.github.com>
2016-05-01 14:52:24 -0700

    updated files

--- JavaScript/Little-Web-Hut-Tutorial/video6code.js
+++ JavaScript/Little-Web-Hut-Tutorial/video6code.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-
+	$("h1").mouseenter(function(){
+		//$(this).css("margin-left", "+=50px"); // increment the margin-left property by 50px
+		// to target multiple css properties, the format looks slightly different:
+		/*$(this).css({
+			"margin-left": "+=50px",
+			"background-color": "red"
+		});
+		*/
+		//$(this).addClass("emphasis shrink"); // adding classes to an element
+		//$(this).removeClass("emphasis"); // removing classes from an element: leave parentheses empty to remove all classes
+		//$(this).removeClass("emphasis").addClass("shrink"); // combining removeClass and addClass to swap classes
+		$(this).toggleClass(); // if no classes are specified in the toggleClass method, any class that's already been specified for an element will be toggled
+	});
 }); // this function ensures that the html has fully run before running the JavaScript
