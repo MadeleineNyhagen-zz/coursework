7a6f1570dd37537b1854a96dabcd65e94fb831d3
Madeleine Nyhagen <m.l.nyhagen@gmail.com>
2016-05-19 16:48:04 -0700

    updated file

--- C#/C#-For-Dummies/Program1/Program1/Program.cs
+++ C#/C#-For-Dummies/Program1/Program1/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Program1
 {
-    class Program
+    public class Program
     {
+        // This is where the program starts.
         static void Main(string[] args)
         {
+            // Prompt user to enter a name:
+            Console.WriteLine("Enter your name please: ");
+
+            // Now read the name entered:
+            string name = Console.ReadLine();
+
+            // Greet the user with the name that was entered:
+            Console.WriteLine("Hello, " + name);
+
+            // Wait for user to acknowledge the results
+            Console.WriteLine("Press enter to terminate...");
+            Console.Read();
+
+            // to insert a code snippet, press ctrl+k and then ctrl+x, use tab to navigate the ensuing menu and enter to select the desired snippet
         }
     }
 }
